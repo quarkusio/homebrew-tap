208004eb7d1d71ab9e64eb01b3ff5802787d89d4
jreleaserbot <jreleaser@kordamp.org>
2022-03-09 21:41:35 +0100

    quarkus 2.7.4.Final

--- Formula/quarkus.rb
+++ Formula/quarkus.rb
@@ -1,20 +1,20 @@
-# Generated with JReleaser 1.0.0-M1 at 2022-03-02T17:05:26.819335+01:00
+# Generated with JReleaser 1.0.0-M3 at 2022-03-09T21:41:27.437642+01:00
 class Quarkus < Formula
   desc "CLI for Quarkus"
   homepage "https://quarkus.io"
-  url "https://github.com/quarkusio/quarkus/releases/download/2.7.3.Final/quarkus-cli-2.7.3.Final.zip"
-  version "2.7.3.Final"
-  sha256 "2cd1beef93311a44a173b8120ef94facdc4c3837ee6387e0f35f1125671ab7b4"
+  url "https://github.com/quarkusio/quarkus/releases/download/2.7.4.Final/quarkus-cli-2.7.4.Final.zip"
+  version "2.7.4.Final"
+  sha256 "fe70a22500c469b790ae505967fd2856b3851c2dad632a43b0d978a0c5dd0ae5"
   license "Apache-2.0"
 
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/quarkus"
+    bin.install_symlink "#{libexec}/bin/quarkus" => "quarkus"
   end
 
   test do
     output = shell_output("#{bin}/quarkus --version")
-    assert_match "2.7.3.Final", output
+    assert_match "2.7.4.Final", output
   end
 end
