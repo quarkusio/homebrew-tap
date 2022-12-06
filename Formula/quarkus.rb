9da8f384e42499390e76202da02e3ea951153889
jreleaserbot <jreleaser@kordamp.org>
2022-12-06 18:16:16 +0100

    quarkus 2.14.3.Final

--- Formula/quarkus.rb
+++ Formula/quarkus.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.3.0 at 2022-11-25T18:12:15.966523+01:00
+# Generated with JReleaser 1.3.0 at 2022-12-06T18:16:07.805118+01:00
 class Quarkus < Formula
   desc "CLI for Quarkus"
   homepage "https://quarkus.io"
-  url "https://github.com/quarkusio/quarkus/releases/download/2.14.2.Final/quarkus-cli-2.14.2.Final.zip"
-  version "2.14.2.Final"
-  sha256 "13ad7165ba4eda9d8618466b8b65bdc41cd547da48bcc8b9781f4d3de0daf707"
+  url "https://github.com/quarkusio/quarkus/releases/download/2.14.3.Final/quarkus-cli-2.14.3.Final.zip"
+  version "2.14.3.Final"
+  sha256 "c39d7194af860db2e04579c66e2f8e7e21c331cf2624eab9ab908c7ad5b11ca1"
   license "Apache-2.0"
 
 
@@ -15,6 +15,6 @@ class Quarkus < Formula
 
   test do
     output = shell_output("#{bin}/quarkus --version")
-    assert_match "2.14.2.Final", output
+    assert_match "2.14.3.Final", output
   end
 end
