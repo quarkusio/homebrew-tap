37415caabd4f448306845dd8005d17b6cb66f508
jreleaserbot <jreleaser@kordamp.org>
2024-03-19 13:57:01 +0000

    quarkus 3.8.3

--- Formula/quarkus.rb
+++ Formula/quarkus.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.3.0 at 2024-03-07T15:44:35.818360004Z
+# Generated with JReleaser 1.3.0 at 2024-03-19T13:56:52.458896817Z
 class Quarkus < Formula
   desc "CLI for Quarkus"
   homepage "https://quarkus.io"
-  url "https://github.com/quarkusio/quarkus/releases/download/3.8.2/quarkus-cli-3.8.2.zip"
-  version "3.8.2"
-  sha256 "b00493e34862d4524b73b24b03076e0a7004a95cfc57482906e90a0c951a9261"
+  url "https://github.com/quarkusio/quarkus/releases/download/3.8.3/quarkus-cli-3.8.3.zip"
+  version "3.8.3"
+  sha256 "8af20d0fb789b9971402db6133fa5e3e9ebc66a99d7f94f023225ade1682a961"
   license "Apache-2.0"
 
 
@@ -15,6 +15,6 @@ class Quarkus < Formula
 
   test do
     output = shell_output("#{bin}/quarkus --version")
-    assert_match "3.8.2", output
+    assert_match "3.8.3", output
   end
 end
